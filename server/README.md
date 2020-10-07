4a902d0c6c7fc8a5ee9ec2ba26812eaa07f7f5e4
NAEK2K <naek2k@outlook.com>
2020-10-07 19:53:02 -0400

    /weekly route

--- server/README.md
+++ server/README.md
@@ -11,3 +11,28 @@
 ### USAGE
 
 run: `python3 server.py`
+
+### DOCUMENTATION
+
+#### /weekly
+
+sends:
+
+json
+
+output:
+
+[
+  {
+    date: epoch_time:int,
+    temp: {
+      c: temp_celsius:int,
+      f: temp_fahrenheit:int,
+      k: temp_kelvin:int
+    },
+    weather: weather_description:str
+  },
+  {
+    ... array contains 7 more consecutive days with the same info, first one being today ...
+  }
+]
--- server/server.py
+++ server/server.py
@@ -1,13 +1,25 @@
 import flask
 import json
 import requests
+import config
+from datetime import datetime
 
 app = flask.Flask(__name__)
+config = config.Config()
 app.config['DEBUG'] = True
 
-@app.route ('/', methods=['GET'])
-def home():
-  api_return = requests.get('https://jsonplaceholder.typicode.com/posts')
-  return flask.Response(json.dumps(api_return.json()), mimetype='application/json')
+@app.route ('/weekly', methods=['GET'])
+def weekly():
+  api_url = 'http://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}'.format('45.424721', '-75.695000', config.OWM_API_KEY)
+  api_return = requests.get(api_url)
+  weather_data = api_return.json()
+  days = []
+  for weather in weather_data['daily']:
+    celsius = round(weather['temp']['day'] - 273.15, 1)
+    fahrenheit = round((weather['temp']['day'] - 273.15) * 9/5 + 32, 1)
+    day = {'date': weather['dt'], 'temp': {'c': celsius, 'f': fahrenheit, 'k': round(weather['temp']['day']}, 1), 'weather': weather['weather'][0]['main']}
+    days.append(day)
+
+  return flask.Response(json.dumps(days), mimetype='application/json')
 
 app.run()
