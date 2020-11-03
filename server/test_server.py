6a3d728dbe5825c63a8cc744722dc6de3bdd4454
NAEK2K <naek2k@outlook.com>
2020-11-03 18:15:11 -0500

    added output to get /task testing

--- server/test_server.py
+++ server/test_server.py
@@ -112,9 +112,11 @@ def test_task_update():
     assert post_task_update().get("status") == 200
 
 
+def test_get_task():
+    response = get_task()
+    print("\nGET: /task {}\n".format(response))
+    assert response.get("status") == 200
+
+
 def test_delete_task():
     assert delete_task().get("status") == 200
-
-
-def test_get_task():
-    assert get_task().get("status") == 200
